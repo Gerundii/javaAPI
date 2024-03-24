cc0b08639aa4feef7b0995520e4038e020d5a8bf
Mikhail Tekuchev <m.tekuchev@gmail.com>
2024-03-24 21:58:17 +0300

    Решение задачи с редиректами

---
+++ src/test/java/RedirectTest.java
@@ -0,0 +1,27 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class RedirectTest {
+    @Test
+    public void testRedirect() {
+        String url = "https://playground.learnqa.ru/api/long_redirect";
+        int statusCode = 0;
+        int count = 0;
+        do {
+            count++;
+            Response response = RestAssured
+                    .given()
+                    .redirects()
+                    .follow(false)
+                    .when()
+                    .get(url)
+                    .andReturn();
+
+            url = response.getHeader("Location");
+            statusCode = response.getStatusCode();
+
+            System.out.println(statusCode + " " + url + " " + count);
+        } while (statusCode != 200);
+    }
+}
