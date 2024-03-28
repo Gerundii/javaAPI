112dc5ff4c11fec5e9d717c0054face0adf77424
Mikhail Tekuchev <m.tekuchev@gmail.com>
2024-03-28 14:15:07 +0300

    Добавил методы генерации пользовательских данных

--- src/test/java/learnQA/util/DataGenerator.java
+++ src/test/java/learnQA/util/DataGenerator.java
@@ -1,10 +1,38 @@
 package learnQA.util;
 
 import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DataGenerator {
     public static String getRandomEmail() {
         String timestamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new java.util.Date());
         return "learnqa" + timestamp + "@example.com";
+    }
+
+    public static Map<String, String> getRegistrationData() {
+        Map<String, String> userData = new HashMap<>();
+        userData.put("email", Datafaker.generateEmail());
+        userData.put("password", "123");
+        userData.put("username", "learnqa");
+        userData.put("firstName", "learn");
+        userData.put("lastName", "learn");
+
+        return userData;
+    }
+
+    public static Map<String, String> getRegistrationData(Map<String, String> nonDefaultValues) {
+        Map<String, String> defaultValues = DataGenerator.getRegistrationData();
+
+        Map<String, String> userData = new HashMap<>();
+        String[] keys = {"email", "password", "username", "firstName", "lastName"};
+        for (String key : keys) {
+            if (nonDefaultValues.containsKey(key)) {
+                userData.put(key, nonDefaultValues.get(key));
+            } else {
+                userData.put(key, defaultValues.get(key));
+            }
+        }
+        return userData;
     }
 }
