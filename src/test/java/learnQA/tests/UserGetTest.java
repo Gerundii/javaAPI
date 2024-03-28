d1ed5dd6e9501317dafd8d9573cc78cff4336603
Mikhail Tekuchev <m.tekuchev@gmail.com>
2024-03-28 10:38:33 +0300

    Добавил тест проверки полученных данных авторизованного пользователя

--- src/test/java/learnQA/tests/UserGetTest.java
+++ src/test/java/learnQA/tests/UserGetTest.java
@@ -6,6 +6,9 @@ import learnQA.lib.Assertions;
 import learnQA.lib.BaseTestCase;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class UserGetTest extends BaseTestCase {
     @Test
     public void testGetUserDataWithoutAuth() {
@@ -17,5 +20,35 @@ public class UserGetTest extends BaseTestCase {
         Assertions.assertJsonHasNotKey(responseUserData, "firstName");
         Assertions.assertJsonHasNotKey(responseUserData, "lastName");
         Assertions.assertJsonHasNotKey(responseUserData, "email");
+    }
+    @Test
+    public void testGetUserDetailsAuthAsSameUser() {
+        Map<String, String> authData = new HashMap<>();
+        authData.put("email", "vinkotov@example.com");
+        authData.put("password", "1234");
+
+        Response responseGetAuth = RestAssured
+                .given()
+                .body(authData)
+                .post("https://playground.learnqa.ru/api/user/login")
+                .andReturn();
+
+        String cookie = getCookie(responseGetAuth, "auth_sid");
+        String header = getHeader(responseGetAuth, "x-csrf-token");
+
+        Response responseUserData = RestAssured
+                .given()
+                .header("x-csrf-token", header)
+                .cookie("auth_sid", cookie)
+                .get("https://playground.learnqa.ru/api/user/2")
+                .andReturn();
+
+        Assertions.assertJsonHasKey(responseUserData, "username");
+        Assertions.assertJsonHasKey(responseUserData, "firstName");
+        Assertions.assertJsonHasKey(responseUserData, "lastName");
+        Assertions.assertJsonHasKey(responseUserData, "email");
+
+
+
     }
 }
