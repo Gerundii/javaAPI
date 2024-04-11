9a48ff369ea88cf3e1fa69f72ef2a354171f92c4
Mikhail Tekuchev <m.tekuchev@gmail.com>
2024-04-11 18:05:02 +0300

    Подправил BaseTestCase
    Добавил создание объекта типа ApiCoreRequests, чтобы не создавать каждый раз в тестовых классах
    Удалил методы hasStatusCode и hasUserIdNotEqualZero

--- src/test/java/learnQA/lib/BaseTestCase.java
+++ src/test/java/learnQA/lib/BaseTestCase.java
@@ -6,32 +6,25 @@ import io.restassured.response.Response;
 import java.util.Map;
 
 import static org.hamcrest.Matchers.hasKey;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BaseTestCase {
-    protected String getHeader (Response response, String name) {
+    protected ApiCoreRequests apiCoreRequests = new ApiCoreRequests();
+
+    protected String getHeader(Response response, String name) {
         Headers headers = response.getHeaders();
         assertTrue(headers.hasHeaderWithName(name), "Response doesn't have header with name " + name);
         return headers.getValue(name);
     }
 
-    protected String getCookie (Response response, String name) {
+    protected String getCookie(Response response, String name) {
         Map<String, String> cookies = response.getCookies();
         assertTrue(cookies.containsKey(name), "Response doesn't have cookie with name " + name);
         return cookies.get(name);
     }
 
-    protected int getIntFromJson (Response response, String name) {
+    protected int getIntFromJson(Response response, String name) {
         response.then().assertThat().body("$", hasKey(name));
         return response.jsonPath().getInt(name);
-    }
-
-    protected void hasStatusCode(Response response, int code) {
-        assertEquals(code, response.statusCode(), "Unexpected status code");
-    }
-
-    protected void hasUserIdNotEqualZero (int usedId) {
-        assertTrue(usedId > 0, "User id should be greater than 0");
     }
 }
