25f07801d9f14d112c51b6e44bc52db91c88d833
Mikhail Tekuchev <m.tekuchev@gmail.com>
2024-04-17 11:46:09 +0300

    Добавил новые методы в Datafaker
    Добавил методы для генерации имени, фамилии и username

--- src/test/java/learnQA/util/Datafaker.java
+++ src/test/java/learnQA/util/Datafaker.java
@@ -11,4 +11,16 @@ public class Datafaker {
         //Faker faker = new Faker(new Locale("ru"));
         return faker.internet().emailAddress();
     }
+    public static String generateFirstName(){
+        Faker faker = new Faker();
+        return faker.name().firstName();
+    }
+    public static String generateLastName(){
+        Faker faker = new Faker();
+        return faker.name().lastName();
+    }
+    public static String generateUserName(){
+        Faker faker = new Faker();
+        return faker.internet().username();
+    }
 }
